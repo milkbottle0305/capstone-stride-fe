4af66ac6f51aae777f49632fce91e467fdb51f33
milkbottle0305 <bigtog0602@gmail.com>
2024-06-23 16:25:17 +0900

    fix: API Endpoint 변경

--- app/src/main/java/com/walktalk/stride/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/walktalk/stride/data/api/RetrofitInstance.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 @SuppressLint("StaticFieldLeak")
 object RetrofitInstance {
     lateinit var context: Context
-    private const val BASE_URL = "http://sonserver.duckdns.org"
+    private const val BASE_URL = "http://sonserver2.duckdns.org"
 
     fun init(context: Context) {
         this.context = context
